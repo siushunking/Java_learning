d276bd62b927c7a7b51e32bfb94bdb19dd48a307
siushunking <siushunkingzz@gmail.com>
2022-12-27 20:41:49 +0800

    constructor

--- src/App.java
+++ src/App.java
@@ -4,18 +4,10 @@ import java.awt.*;
 
 public class App extends JPanel {
     //private static final DecimalFormat DF = new DecimalFormat("0.00");
-    @Override
-    public void paintComponent(Graphics g){
-        //g.drawString("Hello World", 100, 100);
-        g.setColor(Color.BLACK);
-        // g.drawLine(100, 100, 50, 50);
-        g.drawRect(100, 100, 50, 50);
-    }
     public static void main(String[] args) throws Exception {
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(300, 300);
-        window.setContentPane(new App());
-        window.setVisible(true);
+        Circle circle1 = new Circle(10);
+        System.out.println(circle1.radius);
+
+        
     }
 }
--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,7 @@
 public class Circle {
-    
+    public double radius;
+
+    public Circle(double newRadius){
+        radius = newRadius;
+    }
 }
