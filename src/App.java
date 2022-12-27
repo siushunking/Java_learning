6ddbf895ce28b26619b92007880265d98bb61ecc
siushunking <siushunkingzz@gmail.com>
2022-12-27 19:03:19 +0800

    Array sort, get and add

--- src/App.java
+++ src/App.java
@@ -1,19 +1,21 @@
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
+import javax.print.DocFlavor.STRING;
 import javax.swing.JOptionPane;
 
 public class App {
     private static final DecimalFormat DF = new DecimalFormat("0.00");
     public static void main(String[] args) throws Exception {
-       
-
-        String vaccine = JOptionPane.showInputDialog("Hello, Welcome to Javatpoint.");  
-        vaccine = vaccine.toLowerCase();
-
-        if(vaccine.equals("az")){
-            JOptionPane.showMessageDialog(null,"Hello, Welcome to Javatpoint.");  
-        }
-
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("Aike");
+        friends.add("ZGrace");
+        friends.add("Wilson");
+        Collections.sort(friends);
+        System.out.println(friends.get(2));
+        System.out.println(friends);
     }
 }
