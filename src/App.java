603b480c1e28376b6df9fcd5956af6b5c6955219
siushunking <siushunkingzz@gmail.com>
2022-12-27 17:40:30 +0800

    java type

--- src/App.java
+++ src/App.java
@@ -5,6 +5,13 @@ public class App {
 
         String firstName = "IVAN";
         System.out.println(firstName);
+        System.out.println("Where start? " + firstName.charAt(1));
+        System.out.println(firstName.indexOf("AN"));
+
+        String age = "25";
+        System.out.println("convert string to number:" + Integer.parseInt(age));
+
+
         System.out.println("Hello, World!");
     }
 }
