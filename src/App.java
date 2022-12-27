080fcc9316b863cbc40d4420e5c0512838175a77
siushunking <siushunkingzz@gmail.com>
2022-12-27 20:28:16 +0800

    Add ui

--- src/App.java
+++ src/App.java
@@ -1,21 +1,21 @@
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
 
-import javax.print.DocFlavor.STRING;
-import javax.swing.JOptionPane;
-
-public class App {
-    private static final DecimalFormat DF = new DecimalFormat("0.00");
+public class App extends JPanel {
+    //private static final DecimalFormat DF = new DecimalFormat("0.00");
+    @Override
+    public void paintComponent(Graphics g){
+        //g.drawString("Hello World", 100, 100);
+        g.setColor(Color.BLACK);
+        // g.drawLine(100, 100, 50, 50);
+        g.drawRect(100, 100, 50, 50);
+    }
     public static void main(String[] args) throws Exception {
-        ArrayList<String> friends = new ArrayList<>();
-        friends.add("Aike");
-        friends.add("ZGrace");
-        friends.add("Wilson");
-        Collections.sort(friends);
-        System.out.println(friends.get(2));
-        System.out.println(friends);
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(300, 300);
+        window.setContentPane(new App());
+        window.setVisible(true);
     }
 }
