8199a17afdac0ce9d7021ff879c65f2902af9eab
siushunking <siushunkingzz@gmail.com>
2022-12-27 17:30:44 +0800

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        char lastName = 'R';
+        System.out.println(lastName);
+
+        String firstName = "IVAN";
+        System.out.println(firstName);
+        System.out.println("Hello, World!");
+    }
+}
