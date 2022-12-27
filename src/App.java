0ce0980d0dd3e4c74bb69d868214550eb4520477
siushunking <siushunkingzz@gmail.com>
2022-12-27 21:06:39 +0800

    constructorx

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
+import java.util.Scanner;
 
 public class App extends JPanel {
     //private static final DecimalFormat DF = new DecimalFormat("0.00");
@@ -8,6 +9,16 @@ public class App extends JPanel {
         Circle circle1 = new Circle(10);
         System.out.println(circle1.radius);
 
-        
+        Circle circle2 = new Circle();
+        System.out.println(circle2.radius);
+
+        Circle circle3 = new Circle();
+        System.out.println(circle3.getArea());
+
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        Circle circle4 = new Circle(x);
+        System.out.println(circle4.radius);
+
     }
 }
--- src/Circle.java
+++ src/Circle.java
@@ -1,7 +1,15 @@
 public class Circle {
     public double radius;
 
+    public Circle(){
+        radius = 2.5;
+    }
+
     public Circle(double newRadius){
         radius = newRadius;
+    }
+
+    public double getArea(){
+        return Math.PI * radius * radius;
     }
 }
